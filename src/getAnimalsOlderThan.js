ab93b276d08fab7d3e589d09dbdeec697c2ae1b4
Veronica-Alfr <veagalves@gmail.com>
2022-01-25 18:32:04 -0300

    Requisito 2 finalizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const infoAnimal = species.find((nameAnimal) => nameAnimal.name === animal);
+  return infoAnimal.residents.every((animaleAge) => animaleAge.age >= age);
 }
+
+// Ajuda de Tiemi Faustino.
 
 module.exports = getAnimalsOlderThan;
