19a149f866a4d0de1bea79e030fdacb7e1dced2e
Veronica-Alfr <veagalves@gmail.com>
2022-01-26 18:07:48 -0300

    Requisito 9 concluído

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const searchId = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const searchInfos = species.find((animal) => animal.id === searchId).residents;
+  const searchAnimal = searchInfos.reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
+  return Object.values(searchAnimal);
 }
+// Ajuda de Alexandre Summoyama na mentoria do dia, Erick Lima e Lazaro Andriola.
 
 module.exports = getOldestFromFirstSpecies;
