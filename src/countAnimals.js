85c249a30cbede6d2f85a5fde8468c8120ffb797
Veronica-Alfr <veagalves@gmail.com>
2022-01-26 00:40:57 -0300

    Anotação add

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -6,6 +6,7 @@ function countAnimals(animal) {
   const obj = {};
   if (!animal) {
     species.forEach((namesSpecies) => { obj[namesSpecies.name] = namesSpecies.residents.length; });
+    // a linha acima atribui uma chave para obj que é o nome da especie colocada.
     return obj;
   }
   const infoSpecie = species.find((specieAnimal) => specieAnimal.name === animal.specie);
@@ -15,5 +16,5 @@ function countAnimals(animal) {
   return infoSpecie.residents.filter((specieBySex) => specieBySex.sex === animal.sex).length;
 }
 // Ajuda de Imar Mendes.
-console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
+
 module.exports = countAnimals;
