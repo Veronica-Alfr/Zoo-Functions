5b3ffffb4f26feea53c00daf6e6c864de1b785ab
Veronica-Alfr <veagalves@gmail.com>
2022-01-26 00:24:27 -0300

    Requisito 5 concluído

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
 
+function countAnimals(animal) {
+  const obj = {};
+  if (!animal) {
+    species.forEach((namesSpecies) => { obj[namesSpecies.name] = namesSpecies.residents.length; });
+    return obj;
+  }
+  const infoSpecie = species.find((specieAnimal) => specieAnimal.name === animal.specie);
+  if (!animal.sex) {
+    return infoSpecie.residents.length;
+  }
+  return infoSpecie.residents.filter((specieBySex) => specieBySex.sex === animal.sex).length;
+}
+// Ajuda de Imar Mendes.
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 module.exports = countAnimals;
