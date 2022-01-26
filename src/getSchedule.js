1ea6d00ecf0d0389db6630765842c5545a685b99
Veronica-Alfr <veagalves@gmail.com>
2022-01-26 19:42:41 -0300

    Requisito 8 - tópicos 1 e 2 concluídos

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+const { species, hours } = data;
 
+const week = () => {
+  const days = Object.keys(hours);
+  return days.reduce((acc, curr) => {
+    if (curr !== 'Monday') {
+      acc[curr] = {
+        officeHour: `Open from ${hours[curr].open}am until ${hours[curr].close}pm`,
+        exhibition: species.filter((specie) => specie.availability.includes(curr))
+          .map((specie) => specie.name),
+      };
+    } else {
+      acc[curr] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    }
+    return acc;
+  }, {});
+};
+// { 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' }
+console.log(week());
+function getSchedule(scheduleTarget) {
+  return week();
+}
+// Ajuda de Imar Mendes.
 module.exports = getSchedule;
