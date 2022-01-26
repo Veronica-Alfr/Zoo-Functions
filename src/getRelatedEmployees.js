23f75281d6f831355a816f530da6eabbf34c8b95
Veronica-Alfr <veagalves@gmail.com>
2022-01-25 22:37:06 -0300

    Requisito 4 concluído

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((empregado) => empregado.managers.includes(id)); // Ajuda de Danillo Gonçalves
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const infoColaboradores = employees.filter((empregador) =>
+      empregador.managers.includes(managerId));
+    return infoColaboradores.map((contributors) =>
+      `${contributors.firstName} ${contributors.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+
+// Ajuda de Danillo Gonçalves, Raphael Martins e Laís Lametala
 
 module.exports = { isManager, getRelatedEmployees };
