40a235d7ae46bd45fb12527a70044646be766f45
Veronica-Alfr <veagalves@gmail.com>
2022-01-26 02:03:47 -0300

    Requisito 6 concluído

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,34 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const peoples = { child: 0, adult: 0, senior: 0 };
+
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      peoples.child += 1;
+    }
+    if (element.age >= 18 && element.age < 50) {
+      peoples.adult += 1;
+    }
+    if (element.age >= 50) {
+      peoples.senior += 1;
+    }
+  });
+  return peoples;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const infoEntrants = countEntrants(entrants);
+  let valueTotal = 0;
+  valueTotal += prices.child * infoEntrants.child;
+  valueTotal += prices.adult * infoEntrants.adult;
+  valueTotal += prices.senior * infoEntrants.senior;
+  return valueTotal;
 }
-
+// Ajuda de Imar Mendes, Thiago Zardo e Leo Araújo.
 module.exports = { calculateEntry, countEntrants };
