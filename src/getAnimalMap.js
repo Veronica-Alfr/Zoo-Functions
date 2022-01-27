99a04deced8fdd339bec8c30f5ffff1bac26bca5
Veronica-Alfr <veagalves@gmail.com>
2022-01-27 18:47:38 -0300

    Requisito 7, tópicos sobre localização finalizados

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const regions = ['NE', 'NW', 'SE', 'SW'];
+// const [regionNE, regionNW, regionSE, regionSW] = regions;
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options || ({ sex: species.sex === 'female' } && { includeNames: false })
+  || ({ sex: species.sex === 'female', sorted: true })) {
+    return regions.reduce((acc, curr) => {
+      acc[curr] = species.filter(({ location }) => location === curr).map(({ name }) => name);
+      return acc;
+    }, {});
+  }
 }
+
+console.log(getAnimalMap({ sex: 'female', sorted: true }));
 
 module.exports = getAnimalMap;
