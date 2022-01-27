fc02dc2413cbe5afadb758752aabbb8a8ee8e59d
Veronica-Alfr <veagalves@gmail.com>
2022-01-27 20:12:58 -0300

    Alterações na condição if devido o sorted

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,11 +2,10 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 const regions = ['NE', 'NW', 'SE', 'SW'];
-// const [regionNE, regionNW, regionSE, regionSW] = regions;
 
+// O código não lê o valor de sorted
 function getAnimalMap(options) {
-  if (!options || ({ sex: species.sex === 'female' } && { includeNames: false })
-  || ({ sex: species.sex === 'female', sorted: true })) {
+  if (!options || (options.sex === 'female' && (!options.includeNames || options.sorted))) {
     return regions.reduce((acc, curr) => {
       acc[curr] = species.filter(({ location }) => location === curr).map(({ name }) => name);
       return acc;
@@ -14,6 +13,18 @@ function getAnimalMap(options) {
   }
 }
 
-console.log(getAnimalMap({ sex: 'female', sorted: true }));
+const nameAnimals = () => {
+
+};
+
+// Ajuda de Leandro Bonfim
 
 module.exports = getAnimalMap;
+
+// ✕ com a opção `includeNames: true` especificada, retorna nomes de animais (29ms)
+//     ✕ com a opção `sorted: true` especificada, retorna nomes de animais ordenados (14ms)
+//     ✕ com a opção `sex: 'female'` ou `sex: 'male'` especificada,
+//     retorna somente nomes de animais macho/fêmea (9ms)
+//     ✕ com a opção `sex: 'female'` ou `sex: 'male'` especificada e a
+//     opção `sort: true` especificada, retorna somente
+//     nomes de animais macho/fêmea com os nomes dos animais ordenados
