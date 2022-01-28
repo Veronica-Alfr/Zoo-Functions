5bd2b9fcdbe41d880898d1862b3c03dbc3cdfa6b
Veronica-Alfr <veagalves@gmail.com>
2022-01-28 00:42:54 -0300

    Requisito 7 para includeNames em progresso

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,22 +2,32 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 const regions = ['NE', 'NW', 'SE', 'SW'];
+// const animals = ['lions', 'giraffes', 'tigers', 'bears', 'elephants', 'penguins', 'otters',
+//   'frogs', 'snakes'];
 
 // O código não lê o valor de sorted
 function getAnimalMap(options) {
-  if (!options || (options.sex === 'female' && (!options.includeNames || options.sorted))) {
+  if (!options || !options.includeNames) {
     return regions.reduce((acc, curr) => {
       acc[curr] = species.filter(({ location }) => location === curr).map(({ name }) => name);
       return acc;
     }, {});
   }
+  if (options.includeNames) {
+    return regions.reduce((acc, curr) => {
+      acc[curr] = species.filter(({ location }) => location === curr).map(({ name }) => ({ name }));
+      return acc;
+    }, {});
+  }
 }
+// Qualquer coisa muda o nome dos parâmetros e faz tudo separado, dps junta.
+console.log(getAnimalMap({ includeNames: true }));
 
-const nameAnimals = () => {
-
-};
-
-// Ajuda de Leandro Bonfim
+// return animals.reduce((acc, cur) => {
+//   acc[cur] = species.filter(({ residents }) => residents.name === acc).map(({ name }) => name);
+//   return acc;
+// }, {});
+// Ajuda de Leandro Bonfim, Laís Nametala, Danillo Gonçalves
 
 module.exports = getAnimalMap;
 
