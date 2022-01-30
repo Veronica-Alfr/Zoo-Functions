3c90e14bfd5f33b4c8386c27956b2ae90ad47ca3
Veronica-Alfr <veagalves@gmail.com>
2022-01-29 21:39:21 -0300

    Requisito 7, tópico 4 ok

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,10 +2,7 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 const regions = ['NE', 'NW', 'SE', 'SW'];
-// const animals = ['lions', 'giraffes', 'tigers', 'bears', 'elephants', 'penguins', 'otters',
-//   'frogs', 'snakes'];
 
-// O código não lê o valor de sorted
 function getAnimalMap(options) {
   if (!options || !options.includeNames) {
     return regions.reduce((acc, curr) => {
@@ -13,26 +10,19 @@ function getAnimalMap(options) {
       return acc;
     }, {});
   }
-  if (options.includeNames) {
-    return regions.reduce((acc, curr) => {
-      acc[curr] = species.filter(({ location }) => location === curr)
-        .map(({ name }) => ({ [name]: species.map(({ residents }) => residents) }));
-      return acc;
-    }, {});
-  }
+  return regions.reduce((acc, curr) => {
+    acc[curr] = species.filter(({ location }) => location === curr)
+      .map(({ name, residents }) => ({ [name]: residents.map((resident) => resident.name) }));
+    return acc;
+  }, {});
 }
-// Qualquer coisa muda o nome dos parâmetros e faz tudo separado, dps junta.
+
 console.log(getAnimalMap({ includeNames: true }));
 
-// return animals.reduce((acc, cur) => {
-//   acc[cur] = species.filter(({ residents }) => residents.name === acc).map(({ name }) => name);
-//   return acc;
-// }, {});
-// Ajuda de Leandro Bonfim, Laís Nametala, Danillo Gonçalves
+// Ajuda de Leandro Bonfim, Laís Nametala, Danillo Gonçalves, Imar Mendes
 
 module.exports = getAnimalMap;
 
-// ✕ com a opção `includeNames: true` especificada, retorna nomes de animais (29ms)
 //     ✕ com a opção `sorted: true` especificada, retorna nomes de animais ordenados (14ms)
 //     ✕ com a opção `sex: 'female'` ou `sex: 'male'` especificada,
 //     retorna somente nomes de animais macho/fêmea (9ms)
