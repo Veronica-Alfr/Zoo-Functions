ceea068e78e253e78bdfab7fd001db9407d80be3
Veronica-Alfr <veagalves@gmail.com>
2022-01-30 23:45:36 -0300

    Requisito 7 finalizado

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 const { species } = data;
 const regions = ['NE', 'NW', 'SE', 'SW'];
 
-const nameAnimals = (sorted, animal) => regions.reduce((acc, curr) => {
+const nameAnimals = (sorted) => regions.reduce((acc, curr) => {
   acc[curr] = species.filter(({ location }) => location === curr)
     .map(({ name, residents }) => {
       if (!sorted) {
@@ -17,33 +17,40 @@ const nameAnimals = (sorted, animal) => regions.reduce((acc, curr) => {
 const nameBySex = (animal, sorted) => regions.reduce((acc, curr) => {
   acc[curr] = species.filter(({ location }) => location === curr)
     .map(({ name, residents }) => {
-      if (animal.sex) {
-        return ({ [name]: residents.filter(({ sex }) => sex === animal.sex)
-          .map((nameAnimal) => nameAnimal.name) });
+      if (sorted) {
+        return ({ [name]: residents.filter(({ sex }) => sex === animal)
+          .map((nameAnimal) => nameAnimal.name).sort() });
       }
+      return ({ [name]: residents.filter(({ sex }) => sex === animal)
+        .map((nameAnimal) => nameAnimal.name) });
     });
   return acc;
 }, {});
-console.log(nameBySex({ includeNames: true, sex: 'male' }));
+
+const tratativa = (options) => {
+  const { sorted, sex } = options;
+  if (sex && sorted) {
+    return nameBySex(sex, sorted);
+  }
+  if (sorted) {
+    return nameAnimals(sorted);
+  }
+  if (sex) {
+    return nameBySex(sex);
+  }
+  return nameAnimals();
+};
 
 function getAnimalMap(options) {
-  console.log(options);
   if (!options || !options.includeNames) {
     return regions.reduce((acc, curr) => {
       acc[curr] = species.filter(({ location }) => location === curr).map(({ name }) => name);
       return acc;
     }, {});
   }
-  const { sorted } = options;
-  return (options.sorted ? nameAnimals(sorted) : nameAnimals());
+  return (tratativa(options));
 }
 
-// console.log(getAnimalMap({ includeNames: true }));
-
-// Ajuda de Leandro Bonfim, Laís Nametala, Danillo Gonçalves, Imar Mendes, Leo Araújo
+// Ajuda de Leandro Bonfim, Laís Nametala, Danillo Gonçalves, Imar Mendes, Leo Araújo, Sheila Nakashima
 
 module.exports = getAnimalMap;
-
-//     ✕ com a opção `sex: 'female'` ou `sex: 'male'` especificada, retorna somente nomes de animais macho/fêmea
-//     ✕ com a opção `sex: 'female'` ou `sex: 'male'` especificada e a
-//     opção `sort: true` especificada, retorna somente nomes de animais macho/fêmea com os nomes dos animais ordenados
